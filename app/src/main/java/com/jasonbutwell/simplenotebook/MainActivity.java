44fac64abf405ef6ae74fe27d4be9018200f323a
Jason Butwell <jasonbutwell@gmail.com>
2016-10-14 00:03:54 +0100

    '+' New note button added to action bar.

--- app/src/main/java/com/jasonbutwell/simplenotebook/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/simplenotebook/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jasonbutwell.simplenotebook;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        // Check to see if the first Options menu item was selected.
+        if (id == R.id.action_settings) {
+            Log.i("select","add note");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
